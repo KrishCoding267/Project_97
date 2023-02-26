584ea1f0e1596075c5f675521ec6a0fc922692da
KrishCoding267 <113057198+KrishCoding267@users.noreply.github.com>
2023-02-26 17:39:29 +0000

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -5,6 +5,25 @@ function addUser() {
   localStorage.setItem("player1_name", player1_name);
   localStorage.setItem("player2_name", player2_name);
 
-  window.location = "game_page.html";
+  window.location = "index.html";
 }
+function send()
+{
+  number1 = document.getElementById("number1").value;
+  number2 = document.getElementById("number2").value;
+  actual_answer = parseInt(number1) * parseInt(number2);
 
+  question_number = "<h4>" + number1 + " X "+ number2 +"</h4>";
+
+  input_box = "<br>Answer : <input type='text id='input_check_box'>";
+
+  check_button = "<br><br><button class='btn btn-info' onclick='check()>Check</button>";
+
+  row = question_number + input_box + check_button ;
+
+  document.getElementById("output").innerHTML = row;
+
+  document.getElementById("number1").value = "";
+  
+  document.getElementById("number2").value = "";
+}
